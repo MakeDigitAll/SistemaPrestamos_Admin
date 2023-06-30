fc1e9d4593b5da054b29b1d29312f5335c5b9d69
Pablo Alvarez <pablo@makedigitall.com>
2023-06-30 13:03:16 -0600

    v0.4.6.7.4
    Cambios en el body

--- smv_admin/src/components/Body.tsx
+++ smv_admin/src/components/Body.tsx
@@ -12,6 +12,8 @@ interface BodyProps {
   children: React.ReactNode;
 }
 
+type Position = "fixed" | "absolute" | "relative" | "static" | "sticky";
+
 const Body: React.FC<BodyProps> = ({ children }) => {
   const user = useGetAdmin();
   useTokenRenewal();
@@ -24,9 +26,23 @@ const Body: React.FC<BodyProps> = ({ children }) => {
     <div>
       <ToastContainer position="bottom-right" />
       <div style={styles.container}>
-        <SideBar />
-        <div className="header-dashboard-container">
+        <div
+          className="header-sidebar-container"
+          style={styles.headerNavBarContainer}
+        >
           <Header />
+        </div>
+
+        <div
+          className="header-sidebar-container"
+          style={styles.headerSidebarContainer}
+        >
+          <SideBar />
+        </div>
+        <div
+          className="content-footer-container"
+          style={styles.contentFooterContainer}
+        >
           <div style={styles.content}>{children}</div>
           <Footer />
         </div>
@@ -38,11 +54,31 @@ const Body: React.FC<BodyProps> = ({ children }) => {
 const styles = {
   container: {
     display: "grid",
-    gridTemplateColumns: "16.3% auto", // Ajusta los porcentajes según tus necesidades
-    minHeight: "100vh",
+    gridAutoRows: "min-content", // Ajusta el tamaño de las filas según el contenido
+  },
+  headerNavBarContainer: {
+    position: "fixed" as Position,
+    top: 0,
+    left: "16.3%",
+    maxWidth: "100%",
+    zIndex: 1, // Asegura que el encabezado esté por encima del contenido
+  },
+  headerSidebarContainer: {
+    position: "fixed" as Position,
+    top: 0,
+    left: 0,
+    width: "16.3%",
+    height: "100%",
+    zIndex: 1, // Asegura que el encabezado esté por encima del contenido
+  },
+  contentFooterContainer: {
+    marginLeft: "16.3%", // Ajusta según el ancho del sidebar
+    paddingTop: "2%", // Agrega un espacio en la parte superior del contenido
   },
   content: {
-    padding: "0%",
+    padding: "2%",
+    minHeight: "100vh", // Ajusta el alto del contenido
+    marginTop: "2%",
   },
 };
 
